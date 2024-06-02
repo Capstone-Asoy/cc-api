5bcb5a5a90f8d434f83d1f5c90ed3c081d17905c
wdyntr <widyantoro0603@gmail.com>
2024-06-02 09:56:39 +0700

    update bucket

--- storage/upload.js
+++ storage/upload.js
@@ -2,7 +2,7 @@ const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 
 const keyFilename = path.join(__dirname, '../service.json');
-const bucketName = 'ann-user';
+const bucketName = 'profileuser';
 
 const storage = new Storage({ keyFilename });
 const bucket = storage.bucket(bucketName);
