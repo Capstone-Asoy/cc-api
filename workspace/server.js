fb59e81b94161647598aa878f0afd08768d32cf6
quilaxy <annafi.nur.523@gmail.com>
2024-06-07 20:38:27 +0700

    update server

--- workspace/server.js
+++ workspace/server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const bodyParser = require('body-parser')
 const router = require('../routes/routes')
 
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json())
 
 app.use(router)
 // app.use('/auth', router)
 
-app.listen(port, () => {
-	console.log(`Example app listening on port http://localhost:${port}`)
+app.get('/readiness_check', (req, res) => {
+	res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+	console.log(`Example app listening on port http://localhost:${PORT}`)
 })
