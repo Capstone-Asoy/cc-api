e8db20ffffa68be1e260274cb6a75e0829b8e698
wdyntr <widyantoro0603@gmail.com>
2024-06-09 11:17:25 +0700

    update connect

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
-// membuat variable yang membutuhkan node modul mysql
 const mysql = require('mysql')
-// membuat variable db untuk memanggil function yang ada dalam variable mysql
+
 const db = mysql.createConnection({
     host: "localhost", 
     user: "root", 
@@ -13,5 +12,4 @@ const db = mysql.createConnection({
     // database: "backend"
 })
 
-// menjadikan db sebagai modul agar bisa dipakai diluar file connect.js
 module.exports = db
